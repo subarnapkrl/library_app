c1fd7b939ee37490be71baa1f93255bf20e8c2e6
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-10-01 05:39:16 +0545

    Made form section to add books

---
+++ app.js
@@ -0,0 +1,14 @@
+let myLibrary=[];
+
+function Book(title,author,pages,status)
+{
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.status=status;
+}
+
+function addBookToLibrary()
+{
+
+}
